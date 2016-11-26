3023100dbc2e1e2b9539a895abde9c25da843450
Pedro Igor <pedroigorao@hotmail.com>
2016-11-26 13:53:31 -0200

    'teste1'

--- app/src/main/java/com/jlnv/minhastarefas/MainActivity.java
+++ app/src/main/java/com/jlnv/minhastarefas/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-//teste pull request
+//teste pull request 11
 public class MainActivity extends AppCompatActivity {
     private EditText tarefaTexto;
     private Button botaoAdicionar;
